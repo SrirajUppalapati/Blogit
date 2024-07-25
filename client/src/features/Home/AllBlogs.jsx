fc18ef81022fcac4c54806abbde1f7b6800d6470
Sriraj Uppalapati <srirajuppalapati9@gmail.com>
2024-07-25 03:04:07 -0400

    Done with blogs

--- client/src/features/Home/AllBlogs.jsx
+++ client/src/features/Home/AllBlogs.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getBlogs, increasePage } from "./homeSlice";
 import { Spinner } from "flowbite-react";
@@ -9,7 +9,6 @@ function AllBlogs() {
   const dispatch = useDispatch();
   const { blogs, loading, page, filter } = useSelector((state) => state.home);
   const [disable, setDisable] = useState(false);
-  const lastBlogRef = useRef(null);
 
   useEffect(() => {
     dispatch(getBlogs({ page, filter: filter?.tag }))
@@ -19,24 +18,13 @@ function AllBlogs() {
         }
       })
       .catch((err) => toast.error(err));
-  }, [page, filter, dispatch]);
+  }, [page, dispatch, filter]);
 
-  useEffect(() => {
-    if (lastBlogRef.current) {
-      lastBlogRef.current.scrollIntoView({
-        behavior: "smooth",
-        block: "end",
-        inline: "nearest",
-      });
-    }
-  }, [blogs]);
-
-  const handlePage = (e) => {
+  function handlePage(e) {
     e.preventDefault();
     dispatch(increasePage());
-  };
-
-  if (loading && blogs.length === 0) {
+  }
+  if (loading) {
     return (
       <div className="flex justify-center items-center pt-[40%]">
         <Spinner />
@@ -47,15 +35,13 @@ function AllBlogs() {
   return (
     <div className="pb-10 md:pt-20 pt-2">
       {blogs.map((blog, index) => (
-        <div key={index} ref={index === blogs.length - 1 ? lastBlogRef : null}>
-          <BlogCard blog={blog} className="border-0" />
-        </div>
+        <BlogCard blog={blog} key={index} className="border-0" />
       ))}
-      {!disable && (
+      {disable && (
         <button
-          onClick={handlePage}
-          className="pl-10 hover:underline"
-          disabled={loading}
+          onClick={(e) => handlePage(e)}
+          className={`${!disable && "hidden"} pl-10 hover:underline `}
+          disabled={disable}
         >
           Load More
         </button>
